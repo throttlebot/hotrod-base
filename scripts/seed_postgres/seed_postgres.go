e1420a8b71e6eb04bf6eac1965a301d5fa1eab56
Will Wang <will.wang@kelda.io>
2018-06-04 11:55:03 -0700

    inital commit

---
+++ scripts/seed_postgres/seed_postgres.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"os"
+	"fmt"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
+
+	"github.com/kelda/hotrod/services/customer"
+)
+
+var customers = []customer.Customer{
+	{
+		ID:       "123",
+		Name:     "Rachel's Floral Designs",
+		Location: "115,277",
+	}, {
+		ID:       "567",
+		Name:     "Amazing Coffee Roasters",
+		Location: "211,653",
+	}, {
+		ID:       "392",
+		Name:     "Trom Chocolatier",
+		Location: "577,322",
+	}, {
+		ID:       "731",
+		Name:     "Japanese Deserts",
+		Location: "728,326",
+	},
+}
+
+func main() {
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASS")
+	host := os.Getenv("POSTGRESQL_SERVICE_HOST")
+	port := os.Getenv("POSTGRESQL_SERVICE_PORT")
+	connectStr := fmt.Sprintf("postgres://%s:%s@%s:%s?sslmode=disable", user, password, host, port)
+
+	db, err := sql.Open("postgres", connectStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.Exec("CREATE DATABASE customers")
+	// Ignore errors (like if cannot connect, database already exists, etc) until next panic
+
+	connectStr = fmt.Sprintf("postgres://%s:%s@%s:%s/customers?sslmode=disable", user, password, host, port)
+	db, err = sql.Open("postgres", connectStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("DROP TABLE IF EXISTS customers")
+
+	_, err = db.Exec(`CREATE TABLE customers (
+	id varchar(8) PRIMARY KEY,
+	name varchar(64),
+	location varchar(16)
+	)`)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, customer := range customers {
+		_, err = db.Exec("INSERT INTO customers VALUES ($1, $2, $3)",
+			customer.ID, customer.Name, customer.Location)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ scripts/seed_redis/seed_redis.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+
+	"github.com/go-redis/redis"
+
+	"github.com/kelda/hotrod/services/driver"
+)
+
+func main() {
+	redisHost := os.Getenv("REDIS_MASTER_SERVICE_HOST")
+	redisPort := os.Getenv("REDIS_MASTER_SERVICE_PORT")
+	redisPass := os.Getenv("REDIS_MASTER_PASSWORD")
+
+	client := redis.NewClient(&redis.Options{
+		Addr: redisHost + ":" + redisPort,
+		Password: redisPass,
+	})
+
+	var drivers []driver.Driver
+	for i := 0; i < 50; i++ {
+		drivers = append(drivers, driver.Driver{
+			DriverID: fmt.Sprintf("T7%05dC", rand.Int()%100000),
+			Location: fmt.Sprintf("%d,%d", rand.Int()%1000, rand.Int()%1000),
+		})
+	}
+
+	for _, driver := range drivers {
+		if err := client.Set(driver.DriverID, driver.Location, 0).Err(); err != nil {
+			panic(err)
+		}
+	}
+}
