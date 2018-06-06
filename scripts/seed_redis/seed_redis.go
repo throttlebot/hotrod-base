b50c19965e87d756f81fc49394f69c1b918bd188
Will Wang <will.wang@kelda.io>
2018-06-06 13:28:02 -0700

    fixed env hostnames

--- scripts/seed_redis/seed_redis.go
+++ scripts/seed_redis/seed_redis.go
@@ -11,8 +11,8 @@ import (
 )
 
 func main() {
-	redisHost := os.Getenv("HOTROD_REDIS_REDIS_MASTER_SERVICE_HOST")
-	redisPort := os.Getenv("HOTROD_REDIS_REDIS_MASTER_SERVICE_PORT")
+	redisHost := os.Getenv("HOTROD_REDIS_MASTER_SERVICE_HOST")
+	redisPort := os.Getenv("HOTROD_REDIS_MASTER_SERVICE_PORT")
 	redisPass := os.Getenv("REDIS_PASS")
 
 	client := redis.NewClient(&redis.Options{
