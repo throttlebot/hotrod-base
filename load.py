814f7c4d504c2c840aecfb79fae9309a492d62d1
Will Wang <will.wang@kelda.io>
2018-06-22 16:12:02 -0700

    load now is more realistic

--- load.py
+++ load.py
@@ -1,17 +1,42 @@
-import sys, threading, os
+import sys, threading
+import requests, random, time, math
+
+
 customers = [123, 392, 731, 567]
 
-def work(url):
-	os.system("ab -n 100000 -c 1 {}".format(url))
+alive = True
 
-if __name__ == '__main__':
-	host = sys.argv[1]
-	target = "http://{}/dispatch?customer={}&nonse=0.022067955955792473"
 
+def work(host, id):
+	global alive
+	url = "http://" + host + "/"
+	print "Thread {} starting".format(id)
+	while alive:
+		t = time.time()
+		r = requests.get(url)
+		payload = {'customer': str(random.choice(customers)), 'nonse': '0.022067955955792473'}
+		r = requests.get(url + "dispatch", params=payload)
+		time.sleep(max(5 - (time.time() - t), 0))
+	print "Thread {} dying".format(id)
+
+
+def start(host, num_threads):
+	global alive
 	threads = []
-	for i in range(4):
-	    	t = threading.Thread(target=work, args=(target.format(host, customers[i]),))
+	for i in range(num_threads):
+	    	t = threading.Thread(target=work, args=(host, i, ))
     		threads.append(t)
     		t.start()
-    		
-	work("http://" +  host + "/")
+    		time.sleep(random.random() / math.sqrt(num_threads))
+
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		alive = False
+		print "Killing all threads: {}".format(not alive)
+
+
+if __name__ == '__main__':
+	
+	start(sys.argv[1], int(sys.argv[2]))
