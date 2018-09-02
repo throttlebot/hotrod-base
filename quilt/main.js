3c2783b8338fa5b4ff2eca583050907c8593a803
Hantao (Will) Wang <hantaowang97@gmail.com>
2018-09-01 18:28:44 -0700

    Update main.js

--- quilt/main.js
+++ quilt/main.js
@@ -3,15 +3,17 @@ const machineFactory = require('./machines');
 const hotrod = require('./hotrod');
 const placement = require('./placements');
 
-// const namespace = "hotrod-app-" + Math.floor(Math.random() * 10000).toString();
-const namespace = "hotrod-app"
+const namespace = "hotrod-app-" + Math.floor(Math.random() * 10000).toString();
+//const namespace = "hotrod-app"
 const deployment = quilt.createDeployment({namespace: namespace, adminACL: ['0.0.0.0/0']});
 
 var machines = new machineFactory(7);
 // var machines = new machineFactory(21);
 
 hotrodApp = new hotrod();
+
 new placement(hotrodApp, machines.getSizes()).three_per();
+// new placement(hotrodApp, machines.getSizes()).one_per();
 
 deployment.deploy(machines);
 deployment.deploy(hotrodApp);
