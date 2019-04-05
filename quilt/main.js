80d86f916c69c31ff6239dd2c9e2f5e6f7d12fd4
TsaiAnson <ansonytsai@gmail.com>
2019-04-05 20:48:25 +0000

    modifying nginx conf to accept more connections

--- quilt/main.js
+++ quilt/main.js
@@ -2,16 +2,45 @@ const quilt = require('@quilt/quilt');
 const machineFactory = require('./machines');
 const hotrod = require('./hotrod');
 const placement = require('./placements');
+const WorkloadGen = require("./workload.js");
+const utils = require("./utils.js");
 
 // const namespace = "hotrod-app-" + Math.floor(Math.random() * 10000).toString();
-const namespace = "hotrod-app-2"
+const namespace = "hotrod-michael-ubuntu1"
 const deployment = quilt.createDeployment({namespace: namespace, adminACL: ['0.0.0.0/0']});
 
 var machines = new machineFactory(7);
-// var machines = new machineFactory(21);
 
 hotrodApp = new hotrod();
 new placement(hotrodApp, machines.getSizes()).three_per();
 
+const workload_count = 2;
+
+// Create additional machines for worker generation
+machine_list = []
+const baseDiskSize = 20;
+for (i=0; i < workload_count; i++) {
+    const workloadMachine = new quilt.Machine({
+	provider: 'Amazon',
+	size: 'c4.xlarge',
+	diskSize: baseDiskSize+i, 
+	region: 'us-west-1'
+    });
+    utils.addSshKey(workloadMachine);
+    deployment.deploy(workloadMachine.asWorker());
+    machine_list.push(workloadMachine);
+}
+
+const workload = new WorkloadGen(workload_count);
+hotrodApp.ingress.allowFrom(workload.cluster, 80);
+
+workload.cluster[0].placeOn({diskSize: 20});
+for(i=0; i < workload_count; i++) {
+    workload.cluster[i+1].placeOn({diskSize: 20+i});
+    workload.cluster[i+ 1 + workload_count].placeOn({diskSize: 20+i});
+    workload.cluster[i + 1 + workload_count*2].placeOn({diskSize: 20+i});
+}
+
 deployment.deploy(machines);
 deployment.deploy(hotrodApp);
+deployment.deploy(workload);
